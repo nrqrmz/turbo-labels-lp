4bff53253850b37cd5cb2f0a8c15290a382358dd
Enrique Ramirez <nrqrmz@gmail.com>
2024-09-13 05:28:53 -0500

    add colors to labels

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def add_consumer
     @item = Item.find(params[:id])
     @item.consumers << current_user.id
+
     @item.save
 
     respond_to do |format|
@@ -21,6 +22,17 @@ class ItemsController < ApplicationController
     @item.consumers = []
     @item.save
 
-    redirect_to root_path
+    @item.broadcast_item(current_user.id)
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          "item_#{@item.id}",
+          partial: 'items/item',
+          locals: { item: @item }
+        )
+      end
+      format.html { redirect_to root_path }
+    end
   end
 end
